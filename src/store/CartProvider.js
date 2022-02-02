72530c55c1aee5cfe2a87fb46ddb0f2499735bb3
arfath2 <arfath23mohammed@gmail.com>
2022-02-02 19:12:45 +0530

    implementing cart total

---
+++ src/store/CartProvider.js
@@ -0,0 +1,20 @@
+import CartContext from "./CartContext";
+
+const CartProvider =(props)=>{
+    const additemToCartHandler = item =>{}
+
+    const removeItemFromCartHandler = id =>{}
+
+    const cartContext ={
+        items:[],
+        totalAmount: 0,
+        addItem: additemToCartHandler,
+        removeItem:removeItemFromCartHandler,
+    }
+
+    return <CartContext.Provider value={cartContext}>
+        {props.children}
+    </CartContext.Provider>
+}
+
+export default CartProvider;
